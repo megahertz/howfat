152ba517bdcb08d8da8be6602923d97f46cd6e78
Alexey Prokhorov <megahertz@l96.ru>
2022-11-25 14:46:27 +0500

    chore(deps): Update dependencies

--- src/reporters/helpers.js
+++ src/reporters/helpers.js
@@ -15,7 +15,7 @@ function formatSize(bytes) {
   if (bytes === 0) return '0b';
   const e = Math.floor(Math.log(bytes) / Math.log(1024));
   // eslint-disable-next-line no-restricted-properties
-  return +(bytes / (Math.pow(1024, e))).toFixed(2)
+  return +(bytes / (1024 ** e)).toFixed(2)
     + 'bkmgtp'.charAt(e).replace('b', '') + 'b';
 }
 
